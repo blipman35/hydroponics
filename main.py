27a0fe5beee5a77551f669739403e2a2e31b7ff8
blipman35 <benlipman35@gmail.com>
2017-05-09 20:01:01 -0700

    Completed the timer, havent actually connected it to the giop pins yet to make it work

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import datetime
 import time
 
+###Define some constants that determine timing
+WATER_LIGHTS_FOR = 1800 #number of seconds to irrigate and keep the lights on
+WAIT_TIME = 5400 #number of seconds to wait until lights and water are turned on again
+
+def turnon():
+    print('Lights and water are on')
+    #replace with giop pins
+def turnoff():
+    print('Lights and water are off')
+    #replace with giop pins
 while True:
 
     current_time = datetime.datetime.now()
 
-    if current_time.hour % 2 == 0:
-        print('even hour!')
+    turnon()
+    time.sleep(WATER_LIGHTS_FOR)
+
+    turnoff()
+    time.sleep(WATER_LIGHTS_FOR)
